e74a197a2e11404a49972de6bfa1bb7174bf356d
Unknown <chris.kuzma@gmail.com>
2017-11-07 23:10:37 -0800

    Further attempts to normalize the behavior of the ultrasonic sensor

--- arduino/hc_sr04_ultrasonic_test.ino
+++ arduino/hc_sr04_ultrasonic_test.ino
@@ -4,10 +4,13 @@ const int ECHO_PIN = 3;
 
 // Set up limitation for sensor
 const float MAX_DIST_CM = 400.0;
-const float ALARM_DISTANCE_CHANGE = 1.0;
+const float ALARM_DISTANCE_CHANGE = 2.0;
 
-// Set up space to save previous value
-float prev_measure_cm = 0.0;
+// Set up memory buffer for sensor average values
+const int distance_count = 6;
+long recent_distances[distance_count];
+int distance_selector = 0;
+int alarm_worthiness = 0;
 
 void setup() {
   // Ultrasonic sensor trigger pin
@@ -17,13 +20,13 @@ void setup() {
   // Start Serial debugging
   Serial.begin(9600);
 
-  // Delay for sensor to be installed properly
-  Serial.println("Waiting 5 seconds...");
-  delay(5000);
-
   // Get initial reading
   Serial.println("Starting...");
-  prev_measure_cm = getDistance();
+  
+  // Fill the distance memory buffer with initial values
+  for (int x=0; x < distance_count; x++) {
+    recent_distances[x] = getDistance();
+  }
 }
 
 void loop() {
@@ -31,49 +34,73 @@ void loop() {
   float measured_cm = getDistance();
 
   // Print the distance
-  //printDistance(measured_cm);
+  printDistance(measured_cm);
 
   // Check if alarm-worthy
   alarmTest(measured_cm);
-  
-  // Set the new previous value
-  prev_measure_cm = measured_cm;
-  
-  // 60 millis delay
-  delay(60);
-}
 
+  // 150 millis delay
+  delay(150);
+}
 float getDistance() {
-  long t1;
-  long t2;
+    long t1;
+    long t2;
+    float distance;
 
-  // Hold the trigger pin high for 10 millis
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
+    // Hold the trigger pin high for 10 millis
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
 
-  // Wait for pulse on echo pin
-  while ( digitalRead(ECHO_PIN) == 0 );
+    // Wait for pulse on echo pin
+    while ( digitalRead(ECHO_PIN) == 0 );
 
-  // Measure the distance
-  t1 = micros();
-  while ( digitalRead(ECHO_PIN) == 1);
-  t2 = micros();
-  return (t2 - t1) / 58.0;
+    // Measure the distance
+    t1 = micros();
+    while ( digitalRead(ECHO_PIN) == 1);
+    t2 = micros();
+    distance = (t2 - t1) / 58.0;
+    if (distance < MAX_DIST_CM) {
+        return distance;
+    }
+    return MAX_DIST_CM;
 }
-
+  
 void printDistance(float cm) {
-  if (cm > MAX_DIST_CM) {
-    Serial.println("Too far...");
-  } else {
     Serial.print(cm);
     Serial.println(" cm");
-  }
 }
 
-void alarmTest(float new_measure) {
-  if (prev_measure_cm - new_measure > ALARM_DISTANCE_CHANGE || new_measure - prev_measure_cm > ALARM_DISTANCE_CHANGE) {
-    Serial.println("ALARM!!!");
-    delay(2000);
+bool alarmTest(float new_measure) {
+  float average_distance = 0.0;
+  for (int x=0; x < distance_count; x++) {
+    average_distance += recent_distances[x];
   }
+  average_distance = average_distance / distance_count;
+  
+  if (average_distance - new_measure > ALARM_DISTANCE_CHANGE || new_measure - average_distance > ALARM_DISTANCE_CHANGE) {
+    alarm_worthiness += 1;
+  }
+
+  // Add new distance value, update selector
+  recent_distances[distance_selector] = new_measure;
+  if (distance_selector == distance_count) {
+    distance_selector = 0; 
+  }
+
+  // Check if we had enough alarm things go off
+  if (alarm_worthiness == 2) {
+    alarm_worthiness = 0;
+    Serial.println("ALARM!");
+    delay(2000);
+    // Refill the buffer
+    for (int x=0; x < distance_count; x++) {
+      recent_distances[x] = getDistance();
+    }
+    return true;
+  }
+  else {
+    return false;
+  }
+
 }
